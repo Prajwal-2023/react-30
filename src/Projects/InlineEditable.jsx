9bc18c1512f29dbc80a881eb793bae1bf10bb94c
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-04-26 23:58:37 +0530

    Pushed

--- src/Projects/InlineEditable.jsx
+++ src/Projects/InlineEditable.jsx
@@ -1,69 +1,47 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 
 function InlineEditable() {
 
-    const [items, setItems] = useState([
-        { id: 1, name: "userName" },
-        { id: 2, name: "userLastName" }
-    ]);
-
-    const [currentText,setCurrentText] = useState(null)
-    const [currentId,setCurrentId] = useState(null)
-    const inputRef = useRef(null)
-
-    function handleInput(id,name){
-        setCurrentId(id)
-        setCurrentText(name)
-    }
-
-    useEffect(()=>{
-        if(currentId !== null && inputRef.current){
-            inputRef.current.focus()
+    const [details, setDetails] = useState([
+        {
+            id: 1,
+            name: "Username"
+        },
+        {
+            id: 2,
+            name: "Email"
         }
-    },[currentId])
-    
-    function handleBlur(event){
-        if(currentId !== null){
-            saveChanges()
-        }
+    ])
+    const [storeId, setStoreId] = useState(null)
+    const [storeName, setStoreName] = useState(null)
+    const handleClick = (id, name) => {
+        setStoreId(id)
+        setStoreName(name)
     }
-    function handleKeyDown(event){
-        if(event.key === "Enter"){
-            saveChanges()
-        } else if(event.key === "Escape"){
-            setCurrentId(null)
-        }
-
-    }
-
-    function saveChanges(){
-        if(currentId !== null){
-            setItems(items.map(item=> item.id === currentId ? 
-                    {...item, name:currentText} : item))
-                    setCurrentId(null)
-                }
-    }   
+    console.log(storeId, storeName);
 
     return (
         <div className="w-[400px]">
             {/* <input type="text" placeholder="Type here" className="input border-red-700 bg-amber-200 w-[100%] " /> */}
-            {items.map(item => (
-                currentId === item.id ?
-                <input key={item.id} type="text" placeholder="Type here" className="input border-red-700 bg-amber-200 w-[100%] mb-3" 
-                ref={inputRef}
-                onChange={(e)=>setCurrentText(e.target.value)}
-                value={currentText}
-                onBlur={handleBlur}
-                onKeyDown={handleKeyDown}
-               
-                />  : 
-                <div className="w-[100%] bg-green-400 flex items-center justify-between px-2 py-2 mb-3 overflow-x-auto" key={item.id} onClick={()=>handleInput(item.id,item.name)}>
-                    <h1 key={item.id} >{item.name}</h1>
-                    <FaPencilAlt className="cursor-pointer"/>
-                </div>
-            ))} 
-        </div>
+
+
+
+            {details.map((detail) => {
+                return (
+                    storeId === detail.id ?
+                        <input type="text" placeholder="Type here" className="input border-red-700 bg-amber-200 w-[100%] mb-3"
+                        /> :
+                        <div className="w-[100%] bg-green-400 flex items-center justify-between px-2 py-2 mb-3 overflow-x-auto"
+                            key={detail.id} >
+                            <h1>{detail.name}</h1>
+                            <FaPencilAlt className="cursor-pointer"
+                                onClick={() => handleClick(detail.id, detail.name)}
+                            />
+                        </div>)
+                        }
+            )}
+        </div >
     )
 }
 
