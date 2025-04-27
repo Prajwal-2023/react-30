d9165f3c352e28be0cef09fd73ebc13d09c81a85
Prajwal-2023 <PRAJWAL.MCA2302203@SBUP.EDU.IN>
2025-04-27 14:57:24 +0530

    Pushed

--- src/Projects/InlineEditable.jsx
+++ src/Projects/InlineEditable.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { FaPencilAlt } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
+import { FaPencilAlt, FaTrash } from "react-icons/fa";
 
 function InlineEditable() {
 
@@ -13,34 +13,76 @@ function InlineEditable() {
             name: "Email"
         }
     ])
-    const [storeId, setStoreId] = useState(null)
-    const [storeName, setStoreName] = useState(null)
-    const handleClick = (id, name) => {
-        setStoreId(id)
-        setStoreName(name)
-    }
-    console.log(storeId, storeName);
 
+    const [newStoreID, setNewStoreID] = useState(null)
+    const [newStoreValue, setNewStoreValue] = useState(null)
+    const inputRef = useRef(null)
+
+    const handleClick = (id, name) => {
+        setNewStoreID(id)
+        setNewStoreValue(name)
+    }
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            saveChanges()
+        } else if (event.key === "Escape") {
+            setNewStoreID(null)
+        }
+    }
+
+    const saveChanges = () => {
+        if (newStoreID !== null) {
+            setDetails(
+                details.map((deta) =>
+                    deta.id === newStoreID ?
+                        { ...deta, name: newStoreValue } :
+                        deta
+                ));
+        }
+        setNewStoreID(null)
+    }
+
+    const handleDeleteClick = (id) => {
+        setDetails(details.filter(item => item.id !== id));
+    };
+
+    useEffect(() => {
+        if (newStoreID !== null && inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [newStoreID])
     return (
         <div className="w-[400px]">
             {/* <input type="text" placeholder="Type here" className="input border-red-700 bg-amber-200 w-[100%] " /> */}
 
+            {details.map(detail => {
 
-
-            {details.map((detail) => {
                 return (
-                    storeId === detail.id ?
+                    newStoreID === detail.id ?
                         <input type="text" placeholder="Type here" className="input border-red-700 bg-amber-200 w-[100%] mb-3"
-                        /> :
+                            value={newStoreValue}
+                            key={detail.id}
+                            onChange={(e) => setNewStoreValue(e.target.value)}
+                            ref={inputRef}
+                            onKeyDown={handleKeyDown}
+
+                        />
+                        :
                         <div className="w-[100%] bg-green-400 flex items-center justify-between px-2 py-2 mb-3 overflow-x-auto"
-                            key={detail.id} >
+                            key={detail.id}>
                             <h1>{detail.name}</h1>
-                            <FaPencilAlt className="cursor-pointer"
-                                onClick={() => handleClick(detail.id, detail.name)}
-                            />
-                        </div>)
-                        }
-            )}
+                            <div className="flex gap-3"> 
+                                <FaPencilAlt className="cursor-pointer" onClick={() => handleClick(detail.id, detail.name)} />
+                                <FaTrash 
+                                key={detail.id}
+                                className="cursor-pointer"
+                                onClick={()=>handleDeleteClick(detail.id)}
+                                />
+                            </div>
+                        </div>
+                )
+            })}
+
         </div >
     )
 }
